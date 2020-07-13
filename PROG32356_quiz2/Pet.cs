bcd7b33e8870f766dff75676b152ab8bd9afb2a0
Daniel <danielminami@hotmail.com>
2020-07-13 15:39:41 -0400

    Coomit

---
+++ PROG32356_quiz2/Pet.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace PROG32356_quiz2 {
+    public class Pet {
+
+        public string Name { get; set; }
+        public int age { get; set; }
+        public double weight { get; set; }
+        public double height { get; set; }
+
+        public Pet() {
+        }
+    }
+
+}
--- PROG32356_quiz2/Program.cs
+++ PROG32356_quiz2/Program.cs
@@ -8,6 +8,16 @@ namespace PROG32356_quiz2 {
     class Program {
         static void Main(string[] args) {
 
+            /*
+            Barley, age = 8, weight =25, height: 40
+            Pet 2: name = Boots, age = 4, weight =23, height: 42
+            Pet 3: name = Whiskers, age = 1, weight =25, height: 31
+            */
+
+            LinkedList<Pet> PetUnsortedLinkedList = LinkedList<Pet>();
+
+            Pet p1 = new Pet();
+
         }
     }
 }
